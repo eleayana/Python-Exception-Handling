23f527d73681c9a97b683a7e51e7d357742e51b6
eleayana <basneteleayana@gmail.com>
2025-08-30 16:35:56 +0545

    Upload Python Exception handling project

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPythonImportance() {
+  document.getElementById("pythonImportance").innerText =
+    "Python is easy to learn, versatile, and used in web development, AI, data science, automation, and more.";
+}
+
+function showExceptionHandling() {
+  document.getElementById("exceptionExample").innerText =
+    "Example:\ntry {\n  let x = 10 / 0;\n} catch(error) {\n  console.log('Error:', error);\n}";
+}
